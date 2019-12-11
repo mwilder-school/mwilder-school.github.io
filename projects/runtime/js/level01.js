10b5dc07913a4e39c513eb7372953f8f90966b94
mwilder-school <inmwilder@gmail.com>
2019-12-11 01:17:51 +0000

    save all

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,26 +16,26 @@ var level01 = function (window) {
             number: 1,
             speed: -5,
             gameItems: [
-                {type: 'sawblade',x:400,y:groundY},
-                {type: 'sawblade',x:600,y:groundY},
-                {type: 'sawblade',x:900,y:groundY},
-                {type: 'sawblade',x:1150,y:300},
-                {type: 'sawblade',x:1350,y:320},
-                {type: 'sawblade',x:1050,y:400},
-                {type: 'blackhole',x:1000,y:300}
+                {type:'sawblade',x:400,y:groundY},
+                {type:'sawblade',x:600,y:groundY},
+                {type:'sawblade',x:900,y:groundY},
+                {type:'sawblade',x:1150,y:300},
+                {type:'sawblade',x:1350,y:320},
+                {type:'sawblade',x:1050,y:400},
+                {type:'blackhole',x:300,y:300}
             ]
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(false);
+        game.setDebugMode(true);
 
         // BEGIN EDITING YOUR CODE HERE
         for (var i = 0; i < levelData.gameItems.length; i++) {
             var gameItem = levelData.gameItems[i];
-            if (gameItems.type === 'sawblade' ){
+            if (gameItem.type === 'sawblade' ) {
                 createSawBlade(gameItem.x, gameItem.y);
             }
-            else if(gameItems.type === 'blackhole') {
+            else if(gameItem.type === 'blackhole') {
                 createBlackHole(GameItem.x, gameItems.y);
             }
         }
@@ -61,7 +61,17 @@ var level01 = function (window) {
             var obstacleImage = draw.circle(100, 'black', 'lightGray', 2);
             myObstacle.addChild(obstacleImage);
         }
-
+        function createEnemy(x, y) {
+            var enemy = game.createGameItem('enemy', 25);
+            var redSquare = draw.rect(50, 50, 'red');
+            redSquare.x = -25;
+            redSquare.y = -25;
+            enemy.addChild(redSquare);
+            enemy.x = x;
+            enemy.y= y;
+            game.addGameItem(enemy);
+        }
+        createEnemy(350, 200);
     }
 };
 
